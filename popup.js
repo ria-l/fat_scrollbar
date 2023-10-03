af99ce9aa6e1f365a0dd971d20c5e714b305261d
ria-l <55597349+ria-l@users.noreply.github.com>
2023-10-02 20:39:46 -0700

    click a button to make it red

--- popup.js
+++ popup.js
@@ -1,24 +1,23 @@
-// alert('working?');
-// chrome.browserAction.onClicked.addListener(function (tab) {
-//   chrome.tabs.insertCSS({
-//     target: {
-//       tabId: tab.id,
-//     },
-//     css: `body {background-color: pink !important; border: 20px dotted pink; }`,
-//   });
-// });
-chrome.windows.getCurrent(function (currentWindow) {
-  chrome.tabs.query(
-    { active: true, windowId: currentWindow.id },
-    function (activeTabs) {
-      activeTabs.map(function (tab) {
-        chrome.scripting.insertCSS({
-          target: {
-            tabId: tab.id,
-          },
-          files: ['style.css'],
-        });
-      });
-    }
-  );
+async function changeBodyClass() {
+  let queryOptions = { active: true, lastFocusedWindow: true };
+  // `tab` will either be a `tabs.Tab` instance or `undefined`.
+  let [tab] = await chrome.tabs.query(queryOptions);
+
+  // Execute script in the current tab
+  await chrome.scripting.executeScript({
+    target: { tabId: tab.id },
+    func: () => {
+      body = document.getElementsByTagName('body');
+      for (let i = 0; i < body.length; i++) {
+        body[i].classList.add('fatscrollbarsarecool');
+      }
+    },
+  });
+  await chrome.scripting.insertCSS({
+    target: { tabId: tab.id },
+    css: `.fatscrollbarsarecool {::-webkit-scrollbar-thumb {background-color: red !important;}`,
+  });
+}
+document.getElementById('button').addEventListener('click', function () {
+  changeBodyClass();
 });
