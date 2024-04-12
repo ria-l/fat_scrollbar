500e5126429ede0444c1a60f1694551b688b2e07
ria-l <55597349+ria-l@users.noreply.github.com>
2024-04-12 14:56:33 -0700

    add more heart generating options

--- popup.js
+++ popup.js
@@ -1,26 +1,34 @@
-document.getElementById('button').onclick = myf;
+document.getElementById('mirror').addEventListener('click', function() {
+  myf(false, 'mirror');
+});
+document.getElementById('mirror-d').addEventListener('click', function() {
+  myf(true, 'mirror');
+});
+document.getElementById('random').addEventListener('click', function() {
+  myf(false, 'random');
+});
+document.getElementById('random-d').addEventListener('click', function() {
+  myf(true, 'random');
+});
 
-function myf() {
-  let hearts = [
-    '🩷',
-    '❤️',
-    '🧡',
-    '💛',
-    '💚',
-    '🩵',
-    '💙',
-    '💜',
-    '🤎',
-    '🖤',
-    '🩶',
-    '🤍',
-  ];
-  const picks = [];
+function myf(dupe, type) {
+  const hearts = dupe ?
+      [
+        '🩷', '❤️', '🧡', '💛', '💚', '🩵', '💙', '💜', '🖤', '🩶', '🤍', '💖',
+        '🩷', '❤️', '🧡', '💛', '💚', '🩵', '💙', '💜', '🖤', '🩶', '🤍', '💖',
+        '🩷', '❤️', '🧡', '💛', '💚', '🩵', '💙', '💜', '🖤', '🩶', '🤍', '💖',
+      ] :
+      ['🩷', '❤️', '🧡', '💛', '💚', '🩵', '💙', '💜', '🖤', '🩶', '🤍', '💖'];
   let heart_string = '';
-  for (let i = 0; i < 3; i++) {
+  const x = type == 'mirror' ? 3 : 6;
+  for (let i = 0; i < x; i++) {
     const n = Math.floor(Math.random() * hearts.length);
     const picked = hearts.splice(n, 1)[0];
-    heart_string = `${picked}${heart_string}${picked}`;
+    if (type == 'mirror') {
+      heart_string = `${picked}${heart_string}${picked}`;
+    } else {
+      heart_string = `${heart_string}${picked}`;
+    }
+    document.getElementById('hearts').textContent = heart_string;
   }
-  document.getElementById('hearts').textContent = heart_string;
 }
