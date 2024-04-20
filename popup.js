669b4233674710b4582754b484021c133f24ba89
ria-l <55597349+ria-l@users.noreply.github.com>
2024-04-20 09:10:07 -0700

    generate all the heart options at once.

--- popup.js
+++ popup.js
@@ -1,24 +1,51 @@
-document.getElementById('mirror').addEventListener('click', function() {
+document.getElementById('button').addEventListener('click', function () {
   myf(false, 'mirror');
-});
-document.getElementById('mirror-d').addEventListener('click', function() {
   myf(true, 'mirror');
-});
-document.getElementById('random').addEventListener('click', function() {
   myf(false, 'random');
-});
-document.getElementById('random-d').addEventListener('click', function() {
   myf(true, 'random');
 });
 
 function myf(dupe, type) {
-  const hearts = dupe ?
-      [
-        '🩷', '❤️', '🧡', '💛', '💚', '🩵', '💙', '💜', '🖤', '🩶', '🤍', '💖',
-        '🩷', '❤️', '🧡', '💛', '💚', '🩵', '💙', '💜', '🖤', '🩶', '🤍', '💖',
-        '🩷', '❤️', '🧡', '💛', '💚', '🩵', '💙', '💜', '🖤', '🩶', '🤍', '💖',
-      ] :
-      ['🩷', '❤️', '🧡', '💛', '💚', '🩵', '💙', '💜', '🖤', '🩶', '🤍', '💖'];
+  const hearts = dupe
+    ? [
+        '🩷',
+        '❤️',
+        '🧡',
+        '💛',
+        '💚',
+        '🩵',
+        '💙',
+        '💜',
+        '🖤',
+        '🩶',
+        '🤍',
+        '💖',
+        '🩷',
+        '❤️',
+        '🧡',
+        '💛',
+        '💚',
+        '🩵',
+        '💙',
+        '💜',
+        '🖤',
+        '🩶',
+        '🤍',
+        '💖',
+        '🩷',
+        '❤️',
+        '🧡',
+        '💛',
+        '💚',
+        '🩵',
+        '💙',
+        '💜',
+        '🖤',
+        '🩶',
+        '🤍',
+        '💖',
+      ]
+    : ['🩷', '❤️', '🧡', '💛', '💚', '🩵', '💙', '💜', '🖤', '🩶', '🤍', '💖'];
   let heart_string = '';
   const x = type == 'mirror' ? 3 : 6;
   for (let i = 0; i < x; i++) {
@@ -29,6 +56,20 @@ function myf(dupe, type) {
     } else {
       heart_string = `${heart_string}${picked}`;
     }
-    document.getElementById('hearts').textContent = heart_string;
+
+    switch (true) {
+      case type === 'mirror' && dupe === false:
+        document.getElementById('mhnd').textContent = heart_string;
+        break;
+      case type === 'mirror' && dupe === true:
+        document.getElementById('mhd').textContent = heart_string;
+        break;
+      case type != 'mirror' && dupe === false:
+        document.getElementById('rhnd').textContent = heart_string;
+        break;
+      case type != 'mirror' && dupe === true:
+        document.getElementById('rhd').textContent = heart_string;
+        break;
+    }
   }
 }
